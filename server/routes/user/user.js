2eebe34bcaa267157390307f8c6ea8c6c7887304
hrishi-19 <hrishakamath.com>
2023-01-08 13:22:03 +0530

    user route controllers

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -1,9 +1,17 @@
 const router=require('express').Router()
+const cryptoJS =require('crypto-js')
+const{ verfifyTokenAndAuth}=require('../../controller/auth/verifyToken')
+const{ verfifyTokenAndAdmin}=require('../../controller/auth/verifyToken')
+const { updateUser, deleteUser, getSingleUser, getAllUsers, getStats } = require('../../controller/user/user')
+const User = require('../../models/User')
 
+router.put('/:id',verfifyTokenAndAuth,updateUser)
+router.delete('/:id',verfifyTokenAndAuth,deleteUser)
 
-router.get('/',(req,res)=>{
-    res.send("user")
-})
+router.get('/find/:id',verfifyTokenAndAdmin,getSingleUser)
+
+router.get('/',verfifyTokenAndAdmin,getAllUsers)
+router.get('/stats',verfifyTokenAndAdmin,getStats)
 
 
 module.exports=router
