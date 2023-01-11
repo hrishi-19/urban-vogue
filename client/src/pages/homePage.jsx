b200f6a90c18d76276937a317f3aa75809ae0ecb
hrishi-19 <hrishakamath.com>
2023-01-11 19:22:59 +0530

    pages

---
+++ client/src/pages/homePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Flash from '../components/Flash'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
+const HomePage = () => {
+  return (
+    <>
+    <Flash/>
+    <Navbar/>
+    <Slider/>
+   
+    </>
+  )
+}
+
+export default HomePage
